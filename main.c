6dbe887cc7dcd513c917926b9a4a2f65d4953021
zhangcm <zhangcm@ruhrtec.cn>
2024-02-04 16:50:55 +0800

    uart 数据输出

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 
-unsigned char *const UART_BASE = (unsigned char *)0xff570000;
+unsigned char *const UART2_BASE = (unsigned char *)0xff570000;
 
 inline void u8_r(void *addr, unsigned char *val) {
   *val = *((volatile unsigned char *)addr);
@@ -21,21 +21,31 @@ inline void u32_w(void *addr, unsigned int val) {
   *((volatile unsigned int *)addr) = val;
 }
 
+#define UART2_USR (*(volatile unsigned int *)(UART2_BASE + 0x007C))
+
+void uart_write_byte(unsigned char val) 
+{
+  for (; ; ) {
+    unsigned char lsr = 0;
+    u8_r(UART2_BASE + 0x14, &lsr);
+    if (lsr & 0b00100000) {
+      break;
+    }
+  }
+  u8_w(UART2_BASE, val);
+}
+
 void uart_write(void *buffer, int size) {
   unsigned char *pos = buffer;
 
   while (size--) {
-    unsigned char lsr = 0;
-    u8_r(UART_BASE + 5, &lsr);
-    if (lsr & 0b00100000) {
-      u8_w(UART_BASE, *pos);
-    }
+    uart_write_byte(*pos++);
   }
 }
 
 int main() {
   for (;;) {
-    uart_write("AAAA", 4);
+    uart_write("AAAA\n\r", 6);
   }
   return 0;
 }
